a69c484fa0b1ff5dda94f46e4581ca575cf63543
Xavi <javier19pedraza@gmail.com>
2022-03-20 01:08:33 -0500

    finishing cv

--- Colms/app.js
+++ Colms/app.js
@@ -1,38 +1,48 @@
 $(document).ready(function(){
+    $('#menu').click(function(){
+    $(this).toggleClass('fa-times');
+    $('header').toggleClass('toggle');
+    });
 
-        $('#menu').click(function(){
-        $(this).toggleClass('fa-times');
-        $('header').toggleClass('toggle');
-        });
-    
-        $(window).on('scroll load',function(){
-    
+    $(window).on('scroll load',function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
-    
         if($(window).scrollTop() > 0){
             $('.top').show();
         }else{
             $('.top').hide();
         }
-    
-        });
-    
-        // smooth scrolling 
-    
-        $('a[href*="#"]').on('click',function(e){
-    
+    });
+
+    // smooth scrolling 
+
+    $('a[href*="#"]').on('click',function(e){
         e.preventDefault();
-    
         $('html, body').animate({
-    
             scrollTop : $($(this).attr('href')).offset().top,
-    
         },
             500, 
             'linear'
         );
-    
-        });
-    
     });
+});
+
+    const $form = document.querySelector('#form');
+
+    $form.addEventListener('submit', handleSubmit)
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const form = new FormData(this);
+        const response = await fetch(this.action, {
+            method: this.method,
+            body: form,
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        if(response.ok){
+            this.reset()
+            alert("Thanks for your message! I'll be reaching out very soon!.")
+        }
+    }
